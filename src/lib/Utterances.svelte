31937adce079427efdde751c97d20a5e38267f5d
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 15:26:07 +0900

    First commit.

---
+++ src/lib/Utterances.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  export let reponame;
+  export let issueTerm = "pathname";
+  export let label = "comments";
+  export let theme = "github-light";
+  onMount(() => {
+    const s = document.createElement("script");
+    const tag = document.getElementById("utterances");
+    s.setAttribute("repo", reponame);
+    s.setAttribute("issue-term", issueTerm);
+    s.setAttribute("label", label);
+    s.setAttribute("theme", theme);
+    s.setAttribute("crossorigin", "anonymous");
+    s.src = "https://utteranc.es/client.js";
+    tag.parentNode.insertBefore(s, tag);
+  });
+</script>
+
+<div id="utterances" />
