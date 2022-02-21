079d68794c87f6c5f09bf2a1bced15a801a5dafc
Shinichi Okada <okada.shin@gmail.com>
2022-02-21 20:04:10 +0900

    Added top menu.

--- src/routes/doc.svelte
+++ src/routes/doc.svelte
@@ -4,8 +4,33 @@
   import "prism-themes/themes/prism-holi-theme.css";
   import "prismjs/components/prism-markdown.js";
   import { Utterances } from "@codewithshin/svelte-utterances";
+  import { Navbar } from "svelte-flow";
+  let sitename = "Svelte-utterances";
+  let logo = "/images/mkdir-logo.webp";
+  let alt = "Svelte-utterances";
+  let textsize = "text-lg";
+  let menus = [
+    {
+      name: "Home",
+      link: "/",
+      rel: undefined,
+    },
+    {
+      name: "GitHub",
+      link: "https://github.com/shinokada/svelte-utterances",
+      rel: undefined,
+    },
+    {
+      name: "Design",
+      link: "https://svelte-flow.vercel.app",
+      rel: undefined,
+    },
+  ];
 </script>
 
+<div class="max-w-4xl mx-auto py-4">
+  <Navbar {menus} {sitename} {alt} {logo} {textsize} />
+</div>
 <div class="max-w-3xl mx-auto pt-8">
   <div class="container">
     <slot />
