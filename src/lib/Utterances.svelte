b1e71a9ae4e7c57600c3bc7026fcf09e1d03a2ea
Amr <akkk33@protonmail.com>
2022-04-10 16:02:39 +0200

    Fix for #3 : points 1 and 3 Removing `$app/env` and using local variable to be set after the component is mounted Using getElementsByClassName for the iFrame

--- src/lib/Utterances.svelte
+++ src/lib/Utterances.svelte
@@ -1,35 +1,38 @@
 <script>
-	import { onMount } from 'svelte';
-  import { browser } from '$app/env';
+	import { onMount } from "svelte";
 
 	export let reponame;
-	export let issueTerm = 'pathname';
-	export let label = 'comments';
-	export let theme = 'github-light';
+	export let issueTerm = "pathname";
+	export let label = "comments";
+	export let theme = "github-light";
 
 	let scriptElm;
 
-	if (browser) {
-		scriptElm = document.createElement('script');
-		scriptElm.setAttribute('repo', reponame);
-		scriptElm.setAttribute('issue-term', issueTerm);
-		scriptElm.setAttribute('label', label);
-		scriptElm.setAttribute('crossorigin', 'anonymous');
-		scriptElm.src = 'https://utteranc.es/client.js';
-	}
+	let browser = false;
+
 	$: {
 		if (browser) {
 			try {
-				const iFrame = document.getElementsByTagName('iframe')[0];
-				iFrame.contentWindow.postMessage({ type: 'set-theme', theme }, 'https://utteranc.es');
+				const iFrame = document.getElementsByClassName('utterances-frame')[0];
+				iFrame.contentWindow.postMessage(
+					{ type: "set-theme", theme },
+					"https://utteranc.es"
+				);
 			} catch (err) {
 				// The iFrame has not been loaded yet.
 			}
 		}
 	}
 	onMount(() => {
-		const tag = document.getElementById('utterances');
+		scriptElm = document.createElement("script");
+		scriptElm.setAttribute("repo", reponame);
+		scriptElm.setAttribute("issue-term", issueTerm);
+		scriptElm.setAttribute("label", label);
+		scriptElm.setAttribute("crossorigin", "anonymous");
+		scriptElm.src = "https://utteranc.es/client.js";
+		const tag = document.getElementById("utterances");
 		tag.parentNode.insertBefore(scriptElm, tag);
+		browser = true;
 	});
 </script>
 
