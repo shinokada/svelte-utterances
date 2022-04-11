90b974a7fa159d51a18f8163f6f84a59b49d2df0
Shinichi Okada <okada.shin@gmail.com>
2022-04-11 11:04:01 +0900

    fix: add if statement for an error

--- src/lib/Utterances.svelte
+++ src/lib/Utterances.svelte
@@ -1,39 +1,41 @@
 <script>
-	import { onMount } from "svelte";
+  import { onMount } from "svelte";
 
-	export let reponame;
-	export let issueTerm = "pathname";
-	export let label = "comments";
-	export let theme = "github-light";
+  export let reponame;
+  export let issueTerm = "pathname";
+  export let label = "comments";
+  export let theme = "github-light";
 
-	let scriptElm;
+  let scriptElm;
 
-	let browser = false;
+  let browser = false;
 
-	$: {
-		if (browser) {
-			try {
-				const iFrame = document.getElementsByClassName('utterances-frame')[0];
-				iFrame.contentWindow.postMessage(
-					{ type: "set-theme", theme },
-					"https://utteranc.es"
-				);
-			} catch (err) {
-				// The iFrame has not been loaded yet.
-			}
-		}
-	}
-	onMount(() => {
-		scriptElm = document.createElement("script");
-		scriptElm.setAttribute("repo", reponame);
-		scriptElm.setAttribute("issue-term", issueTerm);
-		scriptElm.setAttribute("label", label);
-		scriptElm.setAttribute("crossorigin", "anonymous");
-		scriptElm.src = "https://utteranc.es/client.js";
-		const tag = document.getElementById("utterances");
-		tag.parentNode.insertBefore(scriptElm, tag);
-		browser = true;
-	});
+  $: {
+    if (browser) {
+      try {
+        const iFrame = document.getElementsByClassName("utterances-frame")[0];
+        if (iFrame) {
+          iFrame.contentWindow.postMessage(
+            { type: "set-theme", theme },
+            "https://utteranc.es"
+          );
+        }
+      } catch (err) {
+        // The iFrame has not been loaded yet.
+      }
+    }
+  }
+  onMount(() => {
+    scriptElm = document.createElement("script");
+    scriptElm.setAttribute("repo", reponame);
+    scriptElm.setAttribute("issue-term", issueTerm);
+    scriptElm.setAttribute("label", label);
+    scriptElm.setAttribute("crossorigin", "anonymous");
+    scriptElm.src = "https://utteranc.es/client.js";
+    const tag = document.getElementById("utterances");
+    tag.parentNode.insertBefore(scriptElm, tag);
+    browser = true;
+  });
 </script>
 
 <div id="utterances" />
